dfdb07e2fa4f1b8311757e17f035b15fae78eecc
oopdsc <oopdsc@gmail.com>
2013-01-13 03:36:37 +0800

    Change-Id: I82b2f501a55b15ced6b248803b2d90744bf29b21

---
+++ JWeb/src/com/nono/bean/UserBean.java
@@ -0,0 +1,32 @@
+/**
+ * 
+ */
+package com.nono.bean;
+
+/**
+ * @author nono
+ *
+ */
+public class UserBean {
+	private String name;
+	private String password;
+	
+	public void setName(String name){
+		this.name = name;
+	}
+	
+	public String getName(){
+		return this.name;
+	}
+
+	public String getPassword() {
+		return password;
+	}
+
+	public void setPassword(String password) {
+		this.password = password;
+	}
+	
+	
+
+}
