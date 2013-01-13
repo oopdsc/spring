7b18985e5189591303763872c912ec75d7e81ecc
oopdsc <oopdsc@gmail.com>
2013-01-13 13:51:53 +0800

    ee

---
+++ artifactId/src/main/java/cn/nono/maven/TtttParameter.java
@@ -0,0 +1,11 @@
+package cn.nono.maven;
+
+public class TtttParameter {
+	public int age;
+	public String name;
+
+	public TtttParameter(int age, String name) {
+		this.age = age;
+		this.name = name;
+	}
+}
